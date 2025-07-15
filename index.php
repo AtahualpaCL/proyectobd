4283990bf104697e4698bfefb0ef78ac0903a89f
Atahualpa <zlprofeciaslz@gmail.com>
2025-07-15 03:47:42 -0500

    confirmarReserva

--- index.php
+++ index.php
@@ -2,15 +2,16 @@
 require_once("config.php");
 require_once("controlador/index.php");
 
+$controlador = new modeloController();
+
 if (isset($_GET['m'])) {
     $metodo = $_GET['m'];
-    if (method_exists("modeloController", $metodo)) {
-        modeloController::{$metodo}();
+    if (method_exists($controlador, $metodo)) {
+        $controlador->{$metodo}();
     } else {
         echo "Método no encontrado";
     }
 } else {
-    // Mostrar el selector de tipo de usuario
     require_once("vista/selector_usuario.php");
 }
 ?>
